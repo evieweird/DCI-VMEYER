6802e2e0a4aa150c9acf64525e175df0f88122ef
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 12:52:47 +1000

    init

--- application.py
+++ application.py
@@ -1,10 +1,8 @@
 from flask import Flask
 
-
 application = Flask(__name__)
 
-
-@application.route('/', methods=['GET', 'POST'])
+@application.route('/')
 def hello_world():
     return "Hello World"
 
