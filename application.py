806905d5a31ba1559f662ab4ef5f1348244e610d
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 12:49:15 +1000

    init

--- application.py
+++ application.py
@@ -1,10 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask
 
 
-application = Flask(__name__, template_folder='template')
+application = Flask(__name__)
 
 
 @application.route('/', methods=['GET', 'POST'])
 def hello_world():
-    return render_template('index.html')
+    return "Hello World"
 
