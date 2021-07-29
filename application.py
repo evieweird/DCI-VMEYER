0b077ac3c5f8c693888302750cc8906881a0687c
Veronica Meyer <eveweird@eve-mac.local>
2021-07-29 12:31:06 +1000

    Initial Commit

---
+++ application.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, Markup
+
+
+application = Flask(__name__, template_folder='template')
+
+
+@application.route('/', methods=['GET', 'POST'])
+def hello_world():
+    return render_template('index.html')
+
+@application.route('/database/', methods=['GET', 'POST'])
+def about():
+    return render_template('database.html', data="hi")
